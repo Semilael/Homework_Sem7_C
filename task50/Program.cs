5ae608a01e30a145a28d6c7ae831cc20b1d12595
Semilael <ariana-ses97@yandex.ru>
2022-08-29 21:53:10 +0300

    Added new files

---
+++ task50/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+Console.Write("Please, enter number of rows: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Please, enter number of columns: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 100);
+
+Console.WriteLine($"Good!  We have an array with {columns} columns and {rows} rows. \n\nPlease, enter column and row separated with \"Enter\" to find the element: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+void FindArrayElement(int[,] array)
+{
+    if(a > rows || b > columns)
+    {
+        Console.WriteLine("The number is wrong. Try one more time.");
+    }
+    else
+    {
+        Console.WriteLine(array.GetValue(a,b));
+    }
+}
+
+FindArrayElement(array);
+
+
+
+
+
---
+++ task52/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+Console.Write("Please, enter number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Please, enter number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 100);
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]},  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintArray(array);
+
+
+void FindAverage(int[,] array)
+{
+    Console.WriteLine("Average is:");
+    for (int i = 0; i < columns; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < rows; j++)
+        {
+            sum += array[j, i];
+                       
+        }
+        int average = sum / rows;
+        Console.Write(average + ",  "); 
+    }
+       
+}
+
+FindAverage(array);
