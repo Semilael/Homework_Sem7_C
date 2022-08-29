5fc6534104909fca7f3f94fd013003591c800943
Semilael <ariana-ses97@yandex.ru>
2022-08-29 21:53:28 +0300

    Added new files

--- task47/Program.cs
+++ task47/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+double [,] GetArray(int m, int n, int min, int max)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {  
+            result[i, j] = Convert.ToDouble(new Random().Next(min, max)) / 10;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, 0, 100);
+PrintArray(array);
